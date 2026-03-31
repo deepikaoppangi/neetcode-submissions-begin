c557c2e7c06d95b3a707007f3cf739a5a5e93c36
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2026-03-31 09:50:05 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()){
+            return false;
+        }
+        int n = s.size();
+        unordered_map<char , int> s1;
+        unordered_map<char , int> t1;
+        for(int i=0;i<n;i++){
+            s1[s[i]]++;
+            t1[t[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            char x = s[i];
+            if(s1[x] != t1[x]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+};
