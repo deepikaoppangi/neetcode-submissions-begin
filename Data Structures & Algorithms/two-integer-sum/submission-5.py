86c5eabd1410191d0b5cde2d0949101d16fc82cb
deepikaoppangi <126076250+deepikaoppangi@users.noreply.github.com>
2026-05-14 10:10:24 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,22 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         n = len(nums)
+#         for i in range(0,n):
+#             for j in range(i+1,n):
+#                 if nums[i]+nums[j]==target:
+#                     return [i,j]
+
+#         return []
+        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen={}    ##value , key
+        for i in range(0,len(nums)):
+            complement=target-nums[i]
+
+            if complement in seen:
+                return [seen[complement],i]
+
+            seen[nums[i]]=i
+
+        
